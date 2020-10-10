249dce1abc901ada771f1ed9aa8b2925b9e24701
SarahTaaherBonna <sarahtb25@gmail.com>
2020-10-10 11:22:44 +0800

    Test /db

--- code_base/backend/index.js
+++ code_base/backend/index.js
@@ -16,7 +16,8 @@ app.get("/db", async (req, res) => {
     const client = await pool.connect();
     const result = await client.query("SELECT * FROM test_table");
     const results = { results: result ? result.rows : null };
-    res.render("pages/db", results);
+    // res.render("pages/db", results);
+    res.send("Hello");
     client.release();
   } catch (err) {
     console.error(err);
