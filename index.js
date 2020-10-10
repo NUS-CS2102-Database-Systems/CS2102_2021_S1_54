cfb193856b1d801596d54c5bd81f70339454c6e0
SarahTaaherBonna <sarahtb25@gmail.com>
2020-10-10 11:51:55 +0800

    Test /db v6, test whether database data can be seen

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ app.get("/db", async (req, res) => {
     const client = await pool.connect();
     const result = await client.query("SELECT * FROM test_table");
     const results = { results: result ? result.rows : null };
-    res.render("pages/db", results);
-    // res.send("Hello");
+    // res.render("pages/db", results);
+    res.send(JSON.stringify(results));
     client.release();
   } catch (err) {
     console.error(err);
