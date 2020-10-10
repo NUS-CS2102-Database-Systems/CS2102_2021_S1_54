aa5e0871072afe92f6fd8a275fc9973d8ff4bd3b
SarahTaaherBonna <sarahtb25@gmail.com>
2020-10-10 11:28:28 +0800

    Test /db v2

--- code_base/backend/index.js
+++ code_base/backend/index.js
@@ -2,28 +2,7 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
-const { Pool } = require("pg");
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
-
-app.get("/db", async (req, res) => {
-  try {
-    const client = await pool.connect();
-    const result = await client.query("SELECT * FROM test_table");
-    const results = { results: result ? result.rows : null };
-    // res.render("pages/db", results);
-    res.send("Hello");
-    client.release();
-  } catch (err) {
-    console.error(err);
-    res.send("Error " + err);
-  }
-});
+const PORT = process.env.PORT || 3000;
 
 app.use(function (req, res, next) {
   res.header(
@@ -50,10 +29,32 @@ app.use(express.json());
 // to support URL-encoded bodies
 app.use(express.urlencoded({ extended: true }));
 
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
 app.get("/", function (req, res) {
   res.send("Hello World!");
 });
 
-app.listen(3000, function () {
-  console.log("Example app listening on port 3000!");
+app.get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    // res.render("pages/db", results);
+    res.send("Hello");
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
+app.listen(PORT, function () {
+  console.log("Example app listening on port " + PORT.toString());
 });
