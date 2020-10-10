c911c3760e9247e2564a12534889c26ebc24c99e
SarahTaaherBonna <sarahtb25@gmail.com>
2020-10-10 11:18:34 +0800

    Installed pg, testing it

--- code_base/backend/index.js
+++ code_base/backend/index.js
@@ -3,6 +3,27 @@ const app = express();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
+const { Pool } = require("pg");
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
+});
+
+app.get("/db", async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM test_table");
+    const results = { results: result ? result.rows : null };
+    res.render("pages/db", results);
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+});
+
 app.use(function (req, res, next) {
   res.header(
     "Access-Control-Allow-Headers",
