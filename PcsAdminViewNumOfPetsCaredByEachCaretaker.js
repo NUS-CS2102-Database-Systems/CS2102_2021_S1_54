602cccd6d38f8ece016a1e560b7a557f75cecf11
Ng Guangren, Ryan <e0003902@u.nus.edu>
2020-11-03 17:30:17 +0800

    edited pcsAdmin...

--- PcsAdminViewNumOfPetsCaredByEachCaretaker.js
+++ PcsAdminViewNumOfPetsCaredByEachCaretaker.js
@@ -3,32 +3,28 @@ const app = express();
 
 
 
-app.get("/pcs-admin-total-cared-pets-per-caretaker", async function (
-  req,
-  res
-) {
-  // https://stackoverflow.com/questions/63321982/is-there-a-way-to-send-data-from-the-frontend-to-the-server-with-an-axios-get-re
-  // let pet_owner_details_wanted = req.query.username;
-  const queryNumOfPetsTakenCaredForByEachCaregiver=
-    "SELECT COUNT(*)" +
-    " FROM bid_transaction" +
-    "GROUP BY cusername"+
-    "ORDER BY BY DESC";
+app.get("/pcs-admin-total-cared-pets-per-caretaker", async (req, res) => {
+    // https://stackoverflow.com/questions/63321982/is-there-a-way-to-send-data-from-the-frontend-to-the-server-with-an-axios-get-re
+    // let pet_owner_details_wanted = req.query.username;
+    const queryNumOfPetsTakenCaredForByEachCaregiver = "SELECT COUNT(*)" +
+      " FROM bid_transaction" +
+      "GROUP BY cusername" +
+      "ORDER BY BY DESC";
 
-  try {
-    await client.query(queryNumOfPetsTakenCaredForByEachCaregiver).then((resp) => {
-      console.log("entered num of pets cared for by each caregiver");
-      let arr = [];
-      let len = resp.rows.length;
-      for (var i = 0; i < len; i++) {
-        let result = resp.rows[i];
-        arr.push(result);
-      }
-      res.send(arr);
-    });
-  } catch (err) {
-    console.log("ERROR entered num of pets cared for by each caregiver");
+    try {
+      await client.query(queryNumOfPetsTakenCaredForByEachCaregiver).then((resp) => {
+        console.log("entered num of pets cared for by each caregiver");
+        let arr = [];
+        let len = resp.rows.length;
+        for (var i = 0; i < len; i++) {
+          let result = resp.rows[i];
+          arr.push(result);
+        }
+        res.send(arr);
+      });
+    } catch (err) {
+      console.log("ERROR entered num of pets cared for by each caregiver");
 
-    console.error(err.stack);
-  }
-});
+      console.error(err.stack);
+    }
+  });
