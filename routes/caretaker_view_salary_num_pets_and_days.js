83c8d538e19ad0077755316382614aad62e80146
SarahTaaherBonna <sarahtb25@gmail.com>
2020-12-01 20:39:29 +0800

    Ordered events in pet owner and fulltime caretaker in asc order of job start and end datetime

--- routes/caretaker_view_salary_num_pets_and_days.js
+++ routes/caretaker_view_salary_num_pets_and_days.js
@@ -109,7 +109,8 @@ async function get_current_event_information(req, res) {
     const result = await client.query(
       `SELECT pet_name, pusername, job_start_datetime, job_end_datetime, start_transfer_method, end_transfer_method, payment_method, amount
       FROM bid_transaction 
-      WHERE cusername = '${username}' AND job_start_datetime < '${tomorrow_datetime}' AND job_end_datetime >= '${current_datetime}';`
+      WHERE cusername = '${username}' AND job_start_datetime < '${tomorrow_datetime}' AND job_end_datetime >= '${current_datetime}' 
+      ORDER BY job_start_datetime ASC, job_end_datetime ASC;`
     );
 
     res.setHeader("content-type", "application/json");
@@ -131,7 +132,8 @@ async function get_upcoming_event_information(req, res) {
     const result = await client.query(
       `SELECT pet_name, pusername, job_start_datetime, job_end_datetime, start_transfer_method, end_transfer_method
         FROM bid_transaction 
-        WHERE cusername = '${username}' AND job_start_datetime >= '${tomorrow_datetime}'; `
+        WHERE cusername = '${username}' AND job_start_datetime >= '${tomorrow_datetime}' 
+        ORDER BY job_start_datetime ASC, job_end_datetime ASC; `
     );
 
     res.setHeader("content-type", "application/json");
