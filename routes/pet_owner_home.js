eab4db37added0406a93390a2d7bc7fc6751ad2d
C-E-OOI <e0201208@u.nus.edu>
2020-12-01 20:30:59 +0800

    edited pet owner home

--- routes/pet_owner_home.js
+++ routes/pet_owner_home.js
@@ -15,13 +15,14 @@ async function get_current_event_information(req, res) {
   try {
     const client = await pool.connect();
     const username = req.body.toGet.username;
-    const current_datetime = req.body.toGet.current_datetime;
-    const tomorrow_datetime = req.body.toGet.tomorrow_datetime;
+    const startOfToday_datetime = req.body.toGet.startOfToday_datetime;
+    //const tomorrow_datetime = req.body.toGet.tomorrow_datetime;
+    const moment_datetime = req.body.toGet.moment_datetime;
 
     const result = await client.query(
       `SELECT pet_name, cusername, job_start_datetime, job_end_datetime, start_transfer_method, end_transfer_method, payment_method, amount
       FROM bid_transaction 
-      WHERE pusername = '${username}' AND job_start_datetime < '${tomorrow_datetime}' AND job_end_datetime >= '${current_datetime}';`
+      WHERE pusername = '${username}' AND job_start_datetime < '${moment_datetime}' AND job_end_datetime >= '${startOfToday_datetime}';`
     );
 
     res.setHeader("content-type", "application/json");
