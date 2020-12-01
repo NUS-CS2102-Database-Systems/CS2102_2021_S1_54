6a2d5afb7f8ba8c94024b7f49de72f4170710c14
Liu Zechu <liuzechu2013@gmail.com>
2020-12-01 16:59:53 +0800

    debug hosting

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ const pet_owner_home_route = require("./routes/pet_owner_home.js");
 app.use("/", serveStatic(path.join(__dirname, "/code_base/pcs/dist")));
 
 // this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function (req, res) {
-  res.sendFile(path.join(__dirname, "/code_base/pcs/dist/index.html"));
-});
+// app.get(/.*/, function (req, res) {
+//   res.sendFile(path.join(__dirname, "/code_base/pcs/dist/index.html"));
+// });
 
 app.use(function (req, res, next) {
   res.header(
