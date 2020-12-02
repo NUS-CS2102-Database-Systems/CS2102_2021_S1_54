8d2a07fa193ec1ba3ee383c25f713526a5a62891
C-E-OOI <e0201208@u.nus.edu>
2020-12-03 01:38:15 +0800

    no overlapping bid for the same pet is allow

--- routes/pet_owner_bid_caretakers_routes.js
+++ routes/pet_owner_bid_caretakers_routes.js
@@ -103,6 +103,19 @@ async function submit_a_bid(req, res) {
     const start_transfer_method = req.body.toBid.start_transfer_method;
     const end_transfer_method = req.body.toBid.end_transfer_method;
 
+    var overlapping_bid = await client.query(`
+      SELECT COUNT(*) AS num_overlap
+      FROM bid_transaction 
+      WHERE pet_name = '${pet}' AND pusername = '${username}'
+        AND (job_start_datetime, job_end_datetime) OVERLAPS ('${job_start_datetime}', '${job_end_datetime}');
+      `); //2020-12-02', '2020-12-02'); //sborrownx
+
+    if (overlapping_bid[0].num_overlap >= 1) {
+      res.send("This bid's timeframe overlaps with an existing sucessful bid.");
+      client.release();
+      return;
+    }
+
     // check wether caretaker's pet limit has been reached
     var start = new Date(job_start_datetime);
     start.setHours(0, 0, 0, 0);
